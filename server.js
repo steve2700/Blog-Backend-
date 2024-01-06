7af60f7a09e8140e083798aa0536707ce4102608
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2024-01-06 11:35:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Import userRoutes
+const userRoutes = require('./routes/userRoutes');
+
 dotenv.config();
 
 const app = express();
@@ -15,7 +18,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   //useNewUrlParser: true,
   //useUnifiedTopology: true,
-  //useCreateIndex: true, // Update this line
+  //useCreateIndex: true,
 });
 
 const db = mongoose.connection;
@@ -25,11 +28,12 @@ db.once('open', () => {
   console.log('Connected to MongoDB, you can now start to test your endpoint');
 });
 
-// Define routes
-// Add your routes here
+// Use userRoutes
+app.use('/api/users', userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
 
