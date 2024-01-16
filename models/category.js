65bc844d0a78a76af1cfc0670ddf8fc838c5e704
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2024-01-16 10:29:14 +0200

    Update category.js to deleteOne

--- models/category.js
+++ models/category.js
@@ -11,7 +11,7 @@ const categorySchema = new mongoose.Schema({
 }, { timestamps: true });
 // Define a custom method to delete a category
 categorySchema.methods.deleteCategory = async function () {
-  return this.remove();
+  return this.deleteOne();
 };
 
 const Category = mongoose.model('Category', categorySchema);
