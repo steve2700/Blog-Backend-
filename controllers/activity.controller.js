e7b52e6bdb538e09f1d6e98d95af357deb4e00d9
steve2700 <stevnyaru@github.com>
2024-01-19 23:47:47 +0200

    awesome coding

--- controllers/activity.controller.js
+++ controllers/activity.controller.js
@@ -19,21 +19,21 @@ const activityController = {
     }
   }],
 
-  // List User's Activity Logs
+   // List User's Activity Logs
+
   listUserActivityLogs: [authMiddleware, async (req, res) => {
-    try {
-      const userId = req.user._id;
+  try {
+    const userId = req.user._id;
 
-      // Retrieve activity logs for the specific user
-      const userActivityLogs = await ActivityLog.find({ user: userId });
+    // Retrieve activity logs for the specific user
+    const userActivityLogs = await ActivityLog.find({ user: userId });
 
-      res.status(200).json({ activityLogs: userActivityLogs });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  },
+    res.status(200).json({ activityLogs: userActivityLogs });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}],
 };
 
 module.exports = activityController;
-
