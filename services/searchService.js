494deb86545e6466285b76413743254222295913
steve2700 <stevnyaru@github.com>
2024-01-22 23:39:36 +0200

    removed the func of the createIndex for the search functionality

--- services/searchService.js
+++
@@ -1,7 +0,0 @@
-// services/searchService.js
-const { Client } = require('@elastic/elasticsearch');
-
-const esClient = new Client({ node: 'http://localhost:9200' }); // Update with your Elasticsearch endpoint
-
-module.exports = esClient;
-
