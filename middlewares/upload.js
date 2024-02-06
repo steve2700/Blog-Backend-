ae552d1547d9173338579922a51f984a7d8c35a0
Stewart Nyaruwata <112034196+steve2700@users.noreply.github.com>
2024-02-06 13:23:05 +0200

    Use memory storage forthe upload of profile pictures

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,6 +1,6 @@
 const multer = require('multer');
 
-const storage = multer.memoryStorage(); // Use memory storage for simplicity, adjust as needed
+const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 
 module.exports = upload;
